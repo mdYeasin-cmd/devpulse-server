54e4131d265a663cdf3e0060d2fcc7be2269058e
mdYeasin-cmd <yeasin200011@gmail.com>
2026-06-12 21:13:25 +0600

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,14 @@ import express, {
 import { authRoute } from "./modules/Auth/auth.route";
 import sendResponse from "./utils/sendResponse";
 import { issueRoute } from "./modules/Issue/issue.route";
+import cors from "cors";
 
 const app: Application = express();
+const corsOption = {
+  origin: "http://localhost:5000",
+};
 
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
